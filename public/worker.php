4778b098e2320577fa46a323f78d672517f27a1a
Aditya Rizqi <64444809+adityarizqi@users.noreply.github.com>
2025-07-31 08:55:23 +0700

    feat: make worker.php work dynamic based on server spesification

--- public/worker.php
+++ public/worker.php
@@ -1,11 +1,7 @@
 <?php
 
-$_ENV['APP_ENV'] = 'production';
-$_ENV['APP_BASE_PATH'] = __DIR__ . '/..';
-$_ENV['APP_PUBLIC_PATH'] = __DIR__;
-$_ENV['LARAVEL_OCTANE'] = 1;
-$_ENV['MAX_REQUESTS'] = 1000;
-$_ENV['REQUEST_MAX_EXECUTION_TIME'] = 30;
+$_SERVER['APP_BASE_PATH'] = $_ENV['APP_BASE_PATH'] ?? $_SERVER['APP_BASE_PATH'] ?? __DIR__.'/..';
+$_SERVER['APP_PUBLIC_PATH'] = $_ENV['APP_PUBLIC_PATH'] ?? $_SERVER['APP_BASE_PATH'] ?? __DIR__;
 
 // Make sure you have installed Laravel Octane
 require __DIR__.'/../vendor/laravel/octane/bin/frankenphp-worker.php';
